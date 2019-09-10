49da31f79bd771e9a2e456f740d8b56dfa55c9b7
yueh <purmonth@gmail.com>
2019-09-10 14:04:40 +0800

    complete mnist

--- mnist.py
+++ mnist.py
@@ -1,5 +1,4 @@
 import tensorflow as tf 
-import numpy as np 
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -10,11 +9,13 @@ x = tf.placeholder(dtype=tf.float32, shape=[None,784], name='x')
 #labels
 y = tf.placeholder(dtype=tf.float32, shape=[None,10], name='y')
 
+batch_size = 1000
+
 def add_layer(input_data, input_num, output_num, activation_function=None):
 	#output = input_data * weight + bias
-	w = tf.Variable(initial_value = tf.random_normal(shape=[input_num, output_num]))
-	b = tf.Variable(initial_value = tf.random_normal(shape=[1, output_num]))
-	output = tf.add(tf.matul(input_data, w), b)
+	w = tf.Variable(initial_value=tf.random_normal(shape=[input_num, output_num]))
+	b = tf.Variable(initial_value=tf.random_normal(shape=[1, output_num]))
+	output = tf.add(tf.matmul(input_data, w), b)
 	#activation? output = activation_function(output) : output
 	if activation_function:
 		output = activation_function(output)
@@ -31,12 +32,22 @@ def train_nn(data):
 	# output of NN
 	output = build_nn(data)
 
-	loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output)
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output))
 	optimizer = tf.train.GradientDescentOptimizer(learning_rate=1).minimize(loss)
 
-	for _ in range(50):
-		
 
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		for i in range(50):
+			epoch_cost = 0
+			for _ in range(int(mnist.train.num_examples / batch_size)):
+				x_data, y_data = mnist.train.next_batch(batch_size)
+				cost, _ = sess.run([loss, optimizer], feed_dict={x: x_data, y: y_data})
+				epoch_cost += cost
+			print('Epoch', i, ': ', epoch_cost)
+		accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(output, 1)), tf.float32)) 
+		acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y:mnist.test.labels})
+		print(acc)
 
 
 train_nn(x)
